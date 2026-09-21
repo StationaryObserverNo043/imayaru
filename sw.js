41cd7f71aaae2860f88bdab76a58d509f17adaad
ヒマジン <himajin.neet043@gmail.com>
2026-09-21 10:28:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* イマヤル Service Worker
    ファイルを更新したら CACHE のバージョン番号を上げてください（v1 → v2 ...） */
-const CACHE = 'imayaru-v30';
+const CACHE = 'imayaru-v31';
 const PRECACHE = [
   './',
   './index.html',
